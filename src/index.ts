dd2100a4eb6b68c54eabda70a254de39f4d65779
HommiePeter <peter.scholtens@home.nl>
2021-03-21 16:36:54 +0100

    Revert "0.1 beta"
    This reverts commit 5a4af7f2c6fe7fbc277ea0e2ccf40aa41ac11b1e.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform';
 
-import ToonConfig from './settings';
-import { ToonConnection } from './ToonAPI/ToonConnection';
-import { ToonStatus } from './ToonAPI/ToonDefinitions';
-
-
 /**
  * This method registers the platform with Homebridge
  */
